b8d49f16de57141d22d3e667b8d960ea966fb382
Amitkapila1 <amit.kapila.2009@gmail.com>
2022-04-10 21:24:12 +0530

    commit

--- src/app/homepage/tlindiceslist.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    Data: [
-        { name: 'NIFTY500',value:'NIFTY500/nifty-500/' },
-        { name: 'BANK NIFTY', value: 'NIFTYBANK/nifty-bank/' },
-        {name: 'MIDCAP 100',value:'NIFTYMIDCAP100/nifty-midcap-100/'}
-    ]}
