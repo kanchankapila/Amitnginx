027ce342acfbe217fa192ea4fb64a3eb2a17fae9
Amitkapila1 <amit.kapila.2009@gmail.com>
2022-08-09 22:15:43 +0530

    commit webpack changes

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-    optimization: {
-       concatenateModules: false
-    }
-};
+   
+
+    devServer: {
+       
+           
+            allowedHosts: 'all',
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+                "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+            }
+        }
+    
+}
