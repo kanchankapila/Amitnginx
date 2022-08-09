920258d6f523f8d7fa56fc0f01b13606ac0573c8
Amitkapila1 <amit.kapila.2009@gmail.com>
2022-08-09 11:40:45 +0530

    commit removing unwanted packages

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,26 +3,26 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from "@angular/platform-browser";
 import { DatePipe } from '@angular/common';
-import {FormsModule} from '@angular/forms';
+//import {FormsModule} from '@angular/forms';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavbarModule, ButtonsModule } from 'angular-bootstrap-md'
-import { NgxEchartsModule } from 'ngx-echarts';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+// import { NgxEchartsModule } from 'ngx-echarts';
+ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { DropdownModule } from 'primeng/dropdown';
-import { NgChartsModule } from 'ng2-charts';
+// import { NgChartsModule } from 'ng2-charts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule ,HttpClientJsonpModule} from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MatButtonModule } from '@angular/material/button';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { ChartModule } from 'primeng/chart';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MDBBootstrapModule } from 'angular-bootstrap-md';
+// import { ChartModule } from 'primeng/chart';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { CardModule } from 'primeng/card';
-import { TabViewModule } from 'primeng/tabview';
-import { MatCardModule } from '@angular/material/card';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { CardModule } from 'primeng/card';
+// import { TabViewModule } from 'primeng/tabview';
+ import { MatCardModule } from '@angular/material/card';
 
 
 const routes : Routes = [
@@ -38,28 +38,28 @@ const routes : Routes = [
     SidebarModule,
     HttpClientJsonpModule,
     BrowserAnimationsModule,
-    MatSidenavModule,
+    // MatSidenavModule,
     DropdownModule,
     CommonModule,
     NavbarModule,
-    FormsModule,
+    //FormsModule,
     ButtonsModule,
     NgbModule,
-    TabViewModule,
-    MDBBootstrapModule,
-    NgChartsModule,
+    // TabViewModule,
+    // MDBBootstrapModule,
+    // NgChartsModule,
     HttpClientModule,
-    ChartModule,
+    // ChartModule,
     ButtonModule,
-    MatCardModule,
-    CardModule,
+     MatCardModule,
+    // CardModule,
     BrowserModule,
     //Sidebar1Module,
-    MatButtonModule,
+    // MatButtonModule,
  
    // OwlClockModule,
-    RouterModule.forChild(routes),
-    NgxEchartsModule.forRoot({ echarts: () => import('echarts') }),
+    RouterModule.forChild(routes)
+   
   ],
   exports:[SidebarComponent,NavbarComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
