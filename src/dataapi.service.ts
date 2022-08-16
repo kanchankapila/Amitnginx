3a8bf94750a5c8fb664b173efc5b9cf77d08b717
Amitkapila1 <amit.kapila.2009@gmail.com>
2022-08-16 14:58:41 +0530

    cmt

--- src/dataapi.service.ts
+++ src/dataapi.service.ts
@@ -7,8 +7,8 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 export class DataapiService {
   stock: any;
   dbname: any;
-  //  baseurl = "https://amitv2.herokuapp.com/api"
-   baseurl="http://localhost:3000/api"
+    baseurl = "https://amitv2.herokuapp.com/api"
+  //  baseurl="http://localhost:3000/api"
   constructor(private http: HttpClient,private window: Window,private route: ActivatedRoute,private router: Router) { }
 
 
