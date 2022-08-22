cc81364541c1eaa8c00974d971243066087d1ab2
Amitkapila1 <amit.kapila.2009@gmail.com>
2022-08-22 18:10:01 +0530

    Revert "cmt universal"
    This reverts commit 84d582bca826a6f49607e8c49e2a70591322f8a4.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { environment } from '../environments/environment';
   imports: [
     AppRoutingModule,
     SharedModule,
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
    
