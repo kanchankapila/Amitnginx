40752692568094ad2d982cf6d17a4e7c0310f856
Amitkapila1 <amit.kapila.2009@gmail.com>
2023-04-06 19:49:42 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
+require('chromedriver');
 const express = require('express');
 const fs = require('fs');
 const filePath = './src/app/lists/tlid.txt';
 var app = express();
+const chrome=require('selenium-webdriver/chrome')
 const axios = require('axios');
 const cors = require('cors');
 const path = require('path');
@@ -30,7 +31,148 @@ app.use(bodyParser.raw());
     
     
   });
+  async function def (req, res) {
+   
+    let options = new chrome.Options();
+    //Below arguments are critical for Heroku deployment
+    options.addArguments("--headless");
+    options.addArguments("--disable-gpu");
+   options.addArguments("--no-sandbox");
+   options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH);
+   let serviceBuilder = new chrome.ServiceBuilder(process.env.CHROME_DRIVER_PATH);
+  
+  //  binary_location = "C:\\path\\to\\chrome.exe"
+  //  options.setBinary(".\\src\\assets\\chromedriver.exe");
+  options.addArguments("--disable-dev-shm-usage")
+    let driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(options)
+      .setChromeService(serviceBuilder)
+      .build();
+ 
+    let tabToOpen = driver.get("https://trendlyne.com/visitor/loginmodal/");
+    tabToOpen.then(function () {
+      let findTimeOutP = driver.manage().setTimeouts({ implicit: 5000, });
+      return findTimeOutP;
+    }).then(function () {
+      let promiseUsernameBox = driver.findElement(swd.By.id("id_login"));
+      return promiseUsernameBox;
+    }).then(function (usernameBox) {
+      let promiseFillUsername = usernameBox.sendKeys('amit.kapila.2009@gmail.com');
+      return promiseFillUsername;
+    }).then(function () {
+      console.log("Username entered successfully in Trendlyne");
 
+      let promisePasswordBox = driver.findElement(swd.By.id("id_password"));
+      return promisePasswordBox;
+    }).then(function (passwordBox) {
+      
+      let promiseFillPassword = passwordBox.sendKeys('Angular789\n');
+      return promiseFillPassword;
+    }).then(function () {
+      console.log("Successfully signed in Trendlyne!");
+      driver.manage().getCookie('.trendlyne').then(function (cookiestl) {
+        process.env.trendlynecookietl = cookiestl.value;
+      });
+      driver.manage().getCookie('_gat').then(function (cookiesgat) {
+        
+        process.env.trendlynecookiegat = cookiesgat.value;
+      });
+      driver.manage().getCookie('_gid').then(function (cookiesgid) {
+        process.env.trendlynecookiegid = cookiesgid.value;
+      });
+      driver.manage().getCookie('csrftoken').then(function (cookiescsrf) {
+        process.env.trendlynecookiecsrf = cookiescsrf.value;
+        
+      });
+      driver.manage().getCookie('_ga').then(async function (cookiesga) {
+        process.env.trendlynecookiega = cookiesga.value;
+            
+        process.env.trendlynecookie =
+          '_gid=' + process.env.trendlynecookiegid + '; .trendlyne=' + process.env.trendlynecookietl + '; csrftoken=' + process.env.trendlynecookiecsrf + '; __utma=185246956.775644955.1603113261.1614010114.1614018734.3; _ga=' + process.env.trendlynecookiega + '; _gat=1'
+            
+        // console.log(process.env.trendlynecookie)
+        return (process.env.trendlynecookie),
+        await driver.quit(); 
+      });
+             
+      
+      
+    }).catch(function (err) { console.log("Error ", err, " occurred!"); });
+    //    
+};
+
+      def();
+
+ 
+
+  // * To fetch Opstra session Cookies
+
+// app.get('/api/opstracookie', async 
+async function ghi(req, res) {
+
+    let options1 = new chrome.Options();
+options1.addArguments("--headless");
+options1.addArguments("--disable-gpu");
+  options1.addArguments("--no-sandbox");
+  options1.setChromeBinaryPath(process.env.CHROME_BINARY_PATH);
+   let serviceBuilder1 = new chrome.ServiceBuilder(process.env.CHROME_DRIVER_PATH);
+  
+  //  binary_location = "C:\\path\\to\\chrome.exe"
+  //  options.setBinary(".\\src\\assets\\chromedriver.exe");
+  options1.addArguments("--disable-dev-shm-usage")
+  let driver1 = new webdriver.Builder()
+  .forBrowser('chrome')
+    .setChromeOptions(options1)
+    .setChromeService(serviceBuilder1)
+  .build();
+  
+ 
+  let tabToOpen = driver1.get("https://opstra.definedge.com/ssologin"); 
+  tabToOpen.then(function () { 
+  
+          let findTimeOutP =driver1.manage().setTimeouts({implicit: 5000,}); 
+          return findTimeOutP; }).then(function () { 
+            let promiseUsernameBox = 
+            driver1.findElement(swd.By.id("username")); 
+          return promiseUsernameBox;}).then(function (usernameBox) { 
+  
+          let promiseFillUsername = 
+              usernameBox.sendKeys('amit.kapila.2009@gmail.com'); 
+          return promiseFillUsername; 
+  
+      }).then(function () { 
+        console.log("Username entered successfully in Opstra"); 
+        let promisePasswordBox = 
+      driver1.findElement(swd.By.id("password")); 
+      return promisePasswordBox;}).then(function (passwordBox) { 
+          let promiseFillPassword = 
+          passwordBox.sendKeys('Angular789\n'); 
+          return promiseFillPassword;}).then(function () { 
+      console.log("Successfully signed in Opstra!"); 
+        driver1.manage().getCookie('_gid').then(function (cookiesopgid) {
+          process.env.opstracookiegid=cookiesopgid.value
+        })
+        driver1.manage().getCookie('_gat').then(function (cookiesopgat) {
+            process.env.opstracookiegat=cookiesopgat.value
+        })
+        driver1.manage().getCookie('_ga').then(function (cookiesopga) {
+              process.env.opstracookiega=cookiesopga.value
+        })
+        driver1.manage().getCookie('JSESSIONID').then(async function (cookiesopjsid) {
+                process.env.opstracookiejsid = cookiesopjsid.value
+          process.env.opstracookie = '_ga=' + process.env.opstracookiejsid + '; _gid=' + process.env.opstracookiegid + '; _gat=' + process.env.opstracookiegat + '; JSESSIONID=' + process.env.opstracookiejsid
+              
+                return (process.env.opstracookie),
+               await driver1.quit(); 
+        })
+           }).catch(function (err) { console.log("Error ", err, " occurred!"); });
+          //  await driver1.quit();
+};
+    // );
+ ghi();
+
+ 
   //*This is ET now Stock Data Details used in Share component using parallel api run
   app.get('/api/etsharetoday', function (req, res) {
 
@@ -55,7 +197,7 @@ app.use(bodyParser.raw());
     }
   });
 
- 
+  app.get('/api/et', function (req, res) {
     const start = Date.now();
     const obj=[];
     
@@ -155,7 +297,7 @@ app.use(bodyParser.raw());
   })
 
 
-
+  });
 
   
 
