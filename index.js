7b1e94e49f59f83830a3fe7a083933e4d260fe12
Amitkapila1 <amit.kapila.2009@gmail.com>
2023-04-19 23:17:51 +0530

    cmt

--- index.js
+++ index.js
@@ -446,6 +446,86 @@ app.use(bodyParser.raw());
     });
   });
     
+  app.get('/api/ttvolbreakout', function (req, res) {
+    const start = Date.now();
+    const obj = [];
+  
+    fs.readFile('./tlid.json', async (err, data) => {
+      if (err) {
+        console.log('Error while reading file:', err);
+      
+        return;
+      }
+  
+      try {
+        // Parse the data into an array
+        const symbols = JSON.parse(data);
+  
+        // Process 100 symbols at a time
+        for (let i = 0; i < symbols.length; i += 100) {
+          const symbolBatch = symbols.slice(i, i + 100);
+  
+          const promises = symbolBatch.map(async symbol => {
+            try {
+              const response = await fetch(
+                `https://quotes-api.tickertape.in/quotes?sids=${symbol.ttsymbol}`,
+                {
+                  headers: { Accept: 'application/json' },
+                }
+              );
+  
+              if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+              }
+  
+              const data1 = await response.json();
+              console.log(`${symbol.name}`);
+              console.log(data1['data'][0]['sid'])
+  
+              obj.push({
+                Name: `${symbol.name}`,
+                sid:data1['data'][0]['sid'],
+                volBreakout:data1['data'][0]['volBreakout']
+              });
+            } catch (error) {
+              console.log('Error while fetching data:', error);
+            }
+          });
+  
+          await Promise.all(promises);
+        }
+  
+        const timeTaken = Date.now() - start;
+        console.log(`Total time taken: ${timeTaken} milliseconds`);
+  
+        axiosApiInstance
+          .post('/updateOne', {
+            collection: 'Volume',
+            database: 'Tickertape',
+            dataSource: 'Cluster0',
+            filter: {},
+            update: {
+              $set: {
+                 obj,
+                time: start,
+              },
+            },
+            upsert: true,
+          })
+          .then(() => {
+            console.log('Data updated successfully');
+            res.status(200).send('Data updated successfully');
+          })
+          .catch((error) => {
+            console.log('Error while updating data:', error);
+            res.status(500).send('Error while updating data');
+          });
+      } catch (error) {
+        console.log('Error while parsing data:', error);
+        res.status(500).send('Error while parsing data');
+      }
+    });
+  });
 
   
   app.listen( process.env.PORT || 3000, function () {
